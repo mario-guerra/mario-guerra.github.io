8a4f736dc74841929fa69aa975528e27c1f810f5
Mario Guerra <emailformario@gmail.com>
2025-01-20 15:16:17 -0600

    deploy to gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,21 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
-import mdx from '@astrojs/mdx';
-import { loadEnv } from 'vite'; // Correct import from 'vite'
 import transformLinks from "./vite-plugin-transform-links.mjs"
 
-
-const { PUBLIC_BASE } = loadEnv(import.meta.env.MODE, process.cwd(), '')
+const SERVER_PORT = 3000;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const LIVE_URL = "https://mario-guerra.github.io"; // Add protocol to make it a valid URL
+// this is the astro command your npm script runs
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
 
 export default defineConfig({
   content: {
@@ -16,8 +25,7 @@ export default defineConfig({
       blog: './src/content/blog',  // Path to your blog posts
     },
   },
-  site: "https://mario-guerra.github.io",
-  base: PUBLIC_BASE,
+  site: BASE_URL,
   prefetch: true,
   vite: {
       plugins: [transformLinks()]
